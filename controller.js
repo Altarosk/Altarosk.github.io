ed4eb7a1222b5319ae1c973682a566e71ff70755
Artus Barris <Artus Barris>
2016-03-23 19:46:31 -0400

    \add js

--- controller.js
+++ controller.js
@@ -1,14 +1,16 @@
 //handle nav onclick
-
-var nagivation_elems = document.querySelectorAll(".nav li");
-
-for(var i=0; i<navigation_elems.length; i++){
-	navigation_elems[i].addEventListener("click"), function(evt){
-	var curent_active_nav = document.querySelector(".nav .active");
-	if(curent_active_nav.classList.length>0){
-		current_active_nav.classList.remove("active");
+function init(){
+	var navigation_elems = document.querySelectorAll(".nav li");
+	console.log(navigation_elems);
+	for(var i= 0; i < navigation_elems.length; i++){
+		console.log('hello2');
+		navigation_elems[i].addEventListener("click", function(){
+			var current_active_nav = document.querySelector(".nav .active");
+			if(current_active_nav.classList.length>0){
+				current_active_nav.classList.remove("active");
+			}
+			this.classList.add("active");
+		});
 	}
 
-	this.classList.add("active");
-	});
 }
